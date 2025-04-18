4ca129a03c8f16b706f61d9e1e69962e10d97eb8
priiiyaaa <priyashrestha2004@gmail.com>
2025-04-18 13:03:43 +0545

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,15 @@ import UserDashboard from './pages/UserDashboard.jsx';
 import AboutUs from './pages/AboutUs.jsx';
 import Home from './pages/Home.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import MakePayment from './pages/MakePayment.jsx';
+import PaymentSuccess from './pages/PaymentSuccess.jsx';
+import PaymentFailure from './pages/PaymentFailure.jsx';
+import Statement from './pages/Statement.jsx';
+import FeeDashboard from './pages/FeeDashboard.jsx';
+import Profile from './pages/Profile.jsx';
+import Contact from './pages/Contact.jsx';  
+import VerifyEmail from './pages/VerifyEmail.jsx';
+import ProtectedRoute from './components/ProtectedRoute.jsx';
 
 
 function App() {
